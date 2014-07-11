a5fbd2dc48fade0957f81672e8426739f06ee8c1
Jeff Mataya <jeff.mataya@gmail.com>
2014-07-11 01:29:58 -0400

    Make sign in work for JS, not only HTML

--- lib/controllers/frontend/spree/user_registrations_controller.rb
+++ lib/controllers/frontend/spree/user_registrations_controller.rb
@@ -24,15 +24,15 @@ class Spree::UserRegistrationsController < Devise::RegistrationsController
   def create
     @user = build_resource(spree_user_params)
     if resource.save
+      sign_in(:spree_user, @user)
+      session[:spree_user_signup] = true
+      associate_user
       unless resource.spree_api_key
         resource.generate_spree_api_key!
       end
       respond_to do |format|
         format.html {
           set_flash_message(:notice, :signed_up)
-          sign_in(:spree_user, @user)
-          session[:spree_user_signup] = true
-          associate_user
           sign_in_and_redirect(:spree_user, @user)
         }
         format.js {
